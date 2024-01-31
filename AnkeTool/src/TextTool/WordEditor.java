39d16b2b6b8601eecbc4ea8d8143329c6e94bdad
Yucheng Yao <yaoy68@mcmaster.ca>
2024-01-31 17:55:40 -0500

    javaFX added

---
+++ AnkeTool/src/TextTool/WordEditor.java
@@ -0,0 +1,8 @@
+package TextTool;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+public class WordEditor {
+}
